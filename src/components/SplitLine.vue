c06d75c16461c48d289613ad2ea2684380afa651
NoraH1to <834053207@qq.com>
2022-12-17 13:55:52 +0800

    feat(split-line): split-line 组件

---
+++ src/components/SplitLine.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="b-split-line">
+    <div class="b-split-line__content"><slot /></div>
+  </div>
+</template>
+
+<style lang="scss">
+@use '@/style/mixins.scss' as *;
+.b-split-line {
+  @include flex-center();
+  &__content {
+    color: var(--text3);
+    font-size: var(--fs0);
+  }
+  &::before,
+  &::after {
+    content: '';
+    border-top: 1px solid var(--line_regular);
+    flex-basis: auto;
+    flex-grow: 1;
+    flex-shrink: 1;
+  }
+}
+</style>
