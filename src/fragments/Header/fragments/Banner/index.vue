4dca32b2ac35f089e271931ecb770728cc88953c
NoraH1to <834053207@qq.com>
2022-12-20 12:23:11 +0800

    refactor(banner): 接收参数展示内容

--- src/fragments/Header/fragments/Banner/index.vue
+++ src/fragments/Header/fragments/Banner/index.vue
@@ -1,29 +1,35 @@
 <script lang="ts" setup>
 import Graph from '@/components/Graph/index.vue';
+
+defineProps({
+  hoverText: String,
+  hoverLink: String,
+  logoSrc: {
+    type: String,
+    default:
+      '/i0.hdslb.com/bfs/archive/a349e5844a068d9767d699ab4fdbaa16030af585.png',
+  },
+  coverLink: String,
+  coverSrc: {
+    type: String,
+    default:
+      '/i0.hdslb.com/bfs/archive/f97330ca7905307f539b2af1f64fa2a2fdb4eaed.png@3840w_360h_1c_100q',
+  },
+});
 </script>
 
 <template>
   <div class="b-header-banner">
-    <img
-      class="b-header-banner__cover"
-      src="/i0.hdslb.com/bfs/archive/009c7accbad2e95a27908a18e6cedbd4380a5faf.png@3840w_360h_1c_90q" />
+    <img class="b-header-banner__cover" :src="coverSrc" />
     <div class="b-header-banner__filter"></div>
-    <Graph
-      class="b-header-banner__content"
-      href="https://www.bilibili.com/blackboard/activity-iLmpWZS8SQ.html">
+    <Graph class="b-header-banner__content" :href="coverLink">
       <div class="b-header-banner__info">
         <Graph class="logo-link" href="https://www.bilibili.com"
-          ><img
-            class="logo-img"
-            width="162"
-            height="78"
-            src="/i0.hdslb.com/bfs/archive/c46d6b1ec5f41a4f059cddf7744472a21839f8ee.png"
+          ><img class="logo-img" width="162" height="78" :src="logoSrc"
         /></Graph>
-        <Graph
-          class="text-link"
-          href="www.bilibili.com/blackboard/activity-IQiF1QjSDZ.html?spm_id_from=333.1007.0.0"
-          >好玩的足球都在这，快点我</Graph
-        >
+        <Graph v-if="hoverText" class="text-link" :href="hoverLink">{{
+          hoverText
+        }}</Graph>
       </div>
     </Graph>
   </div>
@@ -77,7 +83,7 @@ import Graph from '@/components/Graph/index.vue';
       border-radius: 4px;
       color: #fff;
       background-color: rgba(47, 50, 56, 0.9);
-      transition: opacity .3s;
+      transition: opacity 0.3s;
       opacity: 0;
     }
   }
